8fa58a81b06126c49c9f34aea345411ccc299f69
SudoNico <nschmalz@hs-mittweida.de>
2024-10-28 09:20:34 +0100

    Update tools.py
    2 tools added

--- tools.py
+++ tools.py
@@ -15,3 +15,17 @@ from sklearn import decomposition, ensemble
 # naive bayes implementation
 from bayesian import Bayes
  
+
+
+#Collection of lightweight text tools for tokenization, word normalization, word segmentation (for splitting hashtags) and spell correction
+#https://github.com/cbaziotis/ekphrasis
+from ekphrasis.classes.preprocessor import TextPreProcessor
+from ekphrasis.classes.tokenizer import SocialTokenizer
+from ekphrasis.dicts.emoticons import emoticons
+from ekphrasis.classes.segmenter import Segmenter
+from ekphrasis.classes.spellcorrect import SpellCorrector
+
+#spaCy comes with pretrained pipelines and currently supports tokenization and training for 70+ languages
+import spacy
+#It features state-of-the-art speed and neural network models for tagging, parsing, named entity recognition, text classification and more
+
