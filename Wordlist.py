11e67e08cc9f14ac3170b62946e31504fd5afd40
SudoNico <nschmalz@hs-mittweida.de>
2024-11-13 12:21:20 +0100

    Update Wordlist.py

--- Wordlist.py
+++ Wordlist.py
@@ -6,24 +6,32 @@ def start():
     with open('German-NRC-EmoLex.csv', 'r') as data:
         for line in data:
             word = line.split()
-            wordlist.append(word[len(word)-1])      
+            if word[len(word)-1] not in wordlist:
+                wordlist.append(word[len(word)-1])
+            row+=1
+
     wordlist.sort()
-    text = [[0 for i in range(12)] for j in range(len(wordlist))]
+    text = [[0 for i in range(12)] for j in range(row)]
+    row=0
+
     with open('German-NRC-EmoLex.csv', 'r') as data:
         for line in data:
             word = line.split() 
             for i in range(12):
                 text[row][i]=word[i]
             row+=1
+            
+
     Labels = [[0 for i in range(11)] for j in range(len(wordlist))]
     for i in range(len(wordlist)):
         Labels[i][0]=wordlist[i]
         for j in range(len(text)):
             if wordlist[i]==text[j][11]:
                 for k in range(1,11):
-                    Labels[i][k]=text[j][k]
+                    Labels[i][k] = int(Labels[i][k]) + int(text[j][k])
                 text.pop(j)                    
                 break
+
     with open('German-NRC-EmoLex-sorted.csv','w') as output:
         csvwriter = csv.writer(output)
         csvwriter.writerows(Labels)    
@@ -33,3 +41,4 @@ def start():
 
 
 start()
+
