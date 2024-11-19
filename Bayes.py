98b410ae0d601b0f42568b80c85d572397057f63
SudoNico <nschmalz@hs-mittweida.de>
2024-11-19 20:42:31 +0100

    Update Bayes.py

--- Bayes.py
+++ Bayes.py
@@ -1,43 +1,39 @@
 import csv
 
-#Quelle Big Data Vorlesung zu NaiveBayes ;)
+# Ich werde noch schauen ob ich eine gute Bibliotek mit fertigen Funktionen finde ansonsten hier erst mal ein commplett selbst aufgesetzter Bayes
+# Quelle Big Data Vorlesung zu NaiveBayes ;)
 
-#words from the text wich needs to be classified
+# words from the input text
 text=[]
 text.append("Sunny")
 text.append("Overcast")
 
-#for which Label? 
+# for which Label? 
 Label=1
 
-#Variables for Summ off all with and without Label
+# Variables for Summ off all with and without Label
 totalyes=0
 totalno=0
 
-#total Summ
-absolute=0
-
-#Calculate total Sum
+# Calculate total Sum
 with open('FrequenzyBSP.csv', 'r') as data:
     reader = csv.reader(data, delimiter=',')
     for line in reader:
         totalyes += int(line[Label])
-        no = 0
         for i in range(1,len(line)):
             if i != Label:
                 totalno += int(line[i])
-                no += int(line[i])
-        absolute = absolute + int(line[Label]) + no
+absolute = totalyes + totalno
 
-#Probability for existance of Label and not Label -- p(Label) and p(notLabel)
+# Probability for existance of Label and not Label -- p(Label) and p(notLabel)
 PLabel = totalyes/absolute
 PnotLabel = totalno/absolute
 
-#Variables for probability -- p(words|Label) and p(words|notLabel)
+# Variables for probability -- p(words|Label) and p(words|notLabel)
 WordLabel = 1
 NotWordLabel = 1
 
-#Calculate p(words|Label) and p(words|notLabel)
+# Calculate p(words|Label) and p(words|notLabel)
 with open('FrequenzyBSP.csv', 'r') as data:
     reader = csv.reader(data, delimiter=',')
     for line in reader:
@@ -52,11 +48,11 @@ with open('FrequenzyBSP.csv', 'r') as data:
                 notword = no/totalno
                 NotWordLabel = NotWordLabel * notword
 
-#calculate final Score
+# calculate final Score
 Bayes = (WordLabel*PLabel)/(NotWordLabel*PnotLabel)
 
 print(Bayes)
 
-#there must be one value for all labels at least else error
-#for testing i used the FrequenzyBSP.csv
+# there must be one value for all labels at least else error
+# for testing i used the FrequenzyBSP.csv
 
