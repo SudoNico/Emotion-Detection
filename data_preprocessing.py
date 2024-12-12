be1543cae071772c578db156691b3e13ad9a175c
Chantal Heubach <chantalheubach@gmail.com>
2024-12-12 10:34:01 +0100

    changed baseline system, updated data_preprocessing and added preprocessed_results

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -62,12 +62,56 @@ def writingFile(text, label):
     with open('/Users/chanti/Desktop/5. Semester/Softwareprojekt/Code/Emotion-Detection/intermediate_results/preprocessed_results_2.txt', 'a', encoding='utf-8') as f:
         f.write(' '.join(text) + ', ' + label + '\n')  # Join the words with spaces and write to file
 
+def writingFile2(text, label1, label2, label3, label4, label5, label6, label7, label8, label9):
+    with open('/Users/chanti/Desktop/5. Semester/Softwareprojekt/Code/Emotion-Detection/intermediate_results/preprocessed_results.txt', 'a', encoding='utf-8') as f:
+        f.write(' '.join(text) + ', ' + label1 + ', ' + label2 + ', ' + label3 + ', ' + label4 + ', ' + label5 + ', ' + label6 + ', ' + label7 + ', ' + label8 + ', ' + label9 + '\n')  # Join the words with spaces and write to file
+
+# 1) preprocessing for binary classification
+
 # loading the csv file
-data = pd.read_csv('/Users/chanti/Desktop/5. Semester/Softwareprojekt/Code/Emotion-Detection/Annotierte Tweets/emo.csv', delimiter=';' ,encoding='utf-8')
-try: 
-    for index, row in data.iterrows(): 
-        s = row['description']
-        l = row['EMO']
+#data = pd.read_csv('/Users/chanti/Desktop/5. Semester/Softwareprojekt/Code/Emotion-Detection/Annotierte Tweets/emo.csv', delimiter=';' ,encoding='utf-8')
+    
+# for index, row in data.iterrows(): 
+#     s = row['description']
+#     l = row['EMO']
+#     corrected_text = spellingCorrection(s, tool)
+    
+#     no_url = urlToLink(corrected_text)
+    
+#     no_punctuation = removeText(no_url)
+    
+#     no_emojis = removeEmoji(no_punctuation)
+
+#     # tokenizing the data into seperate words 
+#     tokens = word_tokenize(no_emojis)
+#     wordpunct_tokenize(no_emojis)
+
+#     filtered_text = removeStopwords()
+
+#     lemmatized_text = lemmatizingText()
+    
+#     # all lower characters 
+#     lower_char = [w.lower() for w in lemmatized_text]
+    
+#     writingFile(lower_char, l)
+ 
+# 2) preprocessing for multi label classification
+
+data2 = pd.read_csv('/Users/chanti/Desktop/5. Semester/Softwareprojekt/Code/Emotion-Detection/Annotierte Tweets/ems.csv', delimiter=';' ,encoding='utf-8')
+
+try:
+       for index, row in data2.iterrows(): 
+        s = str(row['description'])
+        a = str(row['anger'])
+        f = str(row['fear'])
+        su = str(row['surprise'])
+        sa = str(row['sadness'])
+        j = str(row['joy'])
+        d = str(row['disgust'])
+        e = str(row['envy'])
+        je = str(row['jealousy'])
+        o = str(row['other'])
+        
         corrected_text = spellingCorrection(s, tool)
     
         no_url = urlToLink(corrected_text)
@@ -76,7 +120,7 @@ try:
     
         no_emojis = removeEmoji(no_punctuation)
 
-        # tokenizing the data into seperate words 
+     
         tokens = word_tokenize(no_emojis)
         wordpunct_tokenize(no_emojis)
 
@@ -84,13 +128,10 @@ try:
 
         lemmatized_text = lemmatizingText()
     
-        # all lower characters 
+       
         lower_char = [w.lower() for w in lemmatized_text]
     
-        writingFile(lower_char, l)
-        
+        writingFile2(lower_char, a, f, su, sa, j, d, e, je, o)
+
 finally: 
     closeLanguageTool(tool)
- 
-
-
